b9b5974817eae594c3533e6e1a9c47bcd2d37ad4
Omar Cervantes Hernandez <60680930+omarcervantes2599@users.noreply.github.com>
2024-12-09 12:52:40 -0700

    LogIn--Autenticacion con Breeze

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        // VerifyEmail::toMailUsing(function($notifiable, $url){
+        //     return (new MailMessage)
+        //         ->subject('Verificar Cuenta');
+
+        // });
+    }
+}
---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<h1 class="text-4xl">Devs<span class="font-extrabold">Jobs</span></h1>
